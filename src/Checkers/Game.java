70897bdb2bbc24b9ea3dd4e7de5ebfbc8a81ee61
PurplWarrior22 <hodgepw17@gcc.edu>
2018-11-26 19:04:42 -0500

    Apperantly it got changed so Im commiting

--- src/Checkers/Game.java
+++ src/Checkers/Game.java
@@ -61,7 +61,7 @@ public class Game extends JFrame{
 	 */
 	public Game() {
 		//initial board
-		gameBoard = new Board();
+		//gameBoard = new Board();
 		//initial pieces
 		R1 = new Piece(30,130,25,Color.RED);
 		R2 = new Piece(150,130,25,Color.RED);
