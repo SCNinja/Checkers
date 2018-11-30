3a2a0822b06661ebcfc83d24f22f18ebe8ca0f0f
DANENHOWERSL17 <DANENHOWERSL17@5CG7310CQ1.GCC.edu>
2018-11-29 21:32:03 -0500

    Filled in blank spots in 2D array

--- src/Checkers/Board.java
+++ src/Checkers/Board.java
@@ -56,28 +56,59 @@ public class Board extends Game implements MouseListener {
 		this.pieces = new Piece [SIZE][SIZE];
 		Piece whiteSpace = new Piece (0,0,0,Color.WHITE); //filling in a spot where there are no pieces
 		pieces[0][0] = R1;
+		pieces[0][1] = whiteSpace;
 		pieces[0][2] = R2;
+		pieces[0][3] = whiteSpace;
 		pieces[0][4] = R3;
+		pieces[0][5] = whiteSpace;
 		pieces[0][6] = R4;
+		pieces[0][7] = whiteSpace;
+		pieces[1][0] = whiteSpace;
 		pieces[1][1] = R5;
+		pieces[1][2] = whiteSpace;
 		pieces[1][3] = R6;
+		pieces[1][4] = whiteSpace;
 		pieces[1][5] = R7;
+		pieces[1][6] = whiteSpace;
 		pieces[1][7] = R8;
 		pieces[2][0] = R9;
+		pieces[2][1] = whiteSpace;
 		pieces[2][2] = R10;
+		pieces[2][3] = whiteSpace;
 		pieces[2][4] = R11;
+		pieces[2][5] = whiteSpace;
 		pieces[2][6] = R12;
+		pieces[2][7] = whiteSpace;
+		for (int i = 3; i <= 4; i++)
+		{
+			for (int j = 0; j < SIZE; j++)
+			{
+				pieces[i][j] = whiteSpace;
+			}
+		}
+		pieces[5][0] = whiteSpace;
 		pieces[5][1] = B1;
+		pieces[5][2] = whiteSpace;
 		pieces[5][3] = B2;
+		pieces[5][4] = whiteSpace;
 		pieces[5][5] = B3;
+		pieces[5][6] = whiteSpace;
 		pieces[5][7] = B4;
 		pieces[6][0] = B5;
+		pieces[6][1] = whiteSpace;
 		pieces[6][2] = B6;
+		pieces[6][3] = whiteSpace;
 		pieces[6][4] = B7;
+		pieces[6][5] = whiteSpace;
 		pieces[6][6] = B8;
+		pieces[6][7] = whiteSpace;
+		pieces[7][0] = whiteSpace;
 		pieces[7][1] = B9;
+		pieces[7][2] = whiteSpace;
 		pieces[7][3] = B10;
+		pieces[7][4] = whiteSpace;
 		pieces[7][5] = B11;
+		pieces[7][6] = whiteSpace;
 		pieces[7][7] = B12;
 		
 		//Will eventually be commented out?
