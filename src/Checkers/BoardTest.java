4c4862c905be0954f2c4e85bea469d2a944844ba
PurplWarrior22 <hodgepw17@gcc.edu>
2018-11-29 22:33:15 -0500

    Validity Tests

--- src/Checkers/BoardTest.java
+++ src/Checkers/BoardTest.java
@@ -40,14 +40,48 @@ class BoardTest {
 	}
 	
 	@Test
-	public void firstMoveNotValid() {
-		Player player = new Player(Color.BLACK);
-		Click click1 = new Click(80, 430);
-		Click click2 = new Click(80, 590);
+	public void redMoveValidLeft() {
+		Player player = new Player(Color.RED);
+		Click click1 = new Click(4, 2);
+		Click click2 = new Click(3, 3);
 		Board board = new Board();
 		boolean valid = board.valid(player, click1, click2);
-		Assert.assertEquals(false, valid);
+		Assert.assertEquals(true, valid);
 	}
+	
+	@Test
+	public void blackMoveValidLeft() {
+		Player player = new Player(Color.BLACK);
+		Click click1 = new Click(3, 5);
+		Click click2 = new Click(2, 4);
+		Board board = new Board();
+		boolean valid = board.valid(player, click1, click2);
+		Assert.assertEquals(true, valid);
+	}
+	
+	@Test
+	public void redMoveValidRight() {
+		Player player = new Player(Color.RED);
+		Click click1 = new Click(4, 2);
+		Click click2 = new Click(5, 3);
+		Board board = new Board();
+		boolean valid = board.valid(player, click1, click2);
+		Assert.assertEquals(true, valid);
+	}
+	
+	@Test
+	public void blackMoveValidRight() {
+		Player player = new Player(Color.BLACK);
+		Click click1 = new Click(3, 5);
+		Click click2 = new Click(4, 4);
+		Board board = new Board();
+		boolean valid = board.valid(player, click1, click2);
+		Assert.assertEquals(true, valid);
+	}
+	
+	
+	
+	
 	
 	/* IDEAS FOR TESTS
 	 * If a click is on the edge of a board space, make sure it registers and either
