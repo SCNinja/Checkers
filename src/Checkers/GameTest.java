763f55450a6ad85560df49a4325fc999626289ae
PurplWarrior22 <hodgepw17@gcc.edu>
2018-11-30 10:48:56 -0500

    Made x&y pixel tests again in gametest

--- src/Checkers/GameTest.java
+++ src/Checkers/GameTest.java
@@ -7,8 +7,19 @@ import org.junit.jupiter.api.Test;
 class GameTest {
 
 	@Test
-	public void testTest() {
-		Assert.assertEquals(3, 3);
+	public void xPixelToGrid() {
+		Game game = new Game();
+		int click = 100;
+		int xPos = game.xPixelToGridCol(click);
+		Assert.assertEquals(1, xPos);
+	}
+	
+	@Test
+	public void yPixelToGrid() {
+		Game game = new Game();
+		int click = 150;
+		int yPos = game.yPixelToGridRow(click);
+		Assert.assertEquals(0, yPos);
 	}
 	
 	/* IDEAS FOR TESTS
